53e657d7aeddb1cccde58930d3ffbc6540af6553
Nithin Kumar B <nithinkumar5353@gmail.com>
2022-10-28 18:12:05 +0530

    feat: init

---
+++ .wundergraph/wundergraph.config.ts
@@ -0,0 +1,51 @@
+import {
+    Application,
+    configureWunderGraphApplication,
+    cors,
+    EnvironmentVariable,
+    introspect,
+    templates,
+} from '@wundergraph/sdk';
+import server from './wundergraph.server';
+import operations from './wundergraph.operations';
+
+const pokemonAPI = introspect.graphql({
+    apiNamespace: 'pokemon',
+    url: 'https://graphql-pokeapi.graphcdn.app/',
+});
+
+const myApplication = new Application({
+    name: 'app',
+    apis: [
+        pokemonAPI,
+    ],
+});
+
+// configureWunderGraph emits the configuration
+configureWunderGraphApplication({
+    application: myApplication,
+    server,
+    operations,
+    codeGenerators: [
+        {
+            templates: [
+                ...templates.typescript.all,
+                templates.typescript.operations,
+                templates.typescript.linkBuilder,
+            ],
+        },
+    ],
+    cors: {
+        ...cors.allowAll,
+        allowedOrigins:
+            process.env.NODE_ENV === 'production'
+                ? [
+                    // change this before deploying to production to the actual domain where you're deploying your app
+                    'http://localhost:3000',
+                ]
+                : ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+    },
+    dotGraphQLConfig: {
+        hasDotWunderGraphDirectory: false,
+    },
+});
---
+++ README.md
@@ -0,0 +1,31 @@
+# Simple starter for WunderGraph Cloud
+
+A simple starter that consumes the [Pokémon GraphQL API](https://graphql-pokeapi.vercel.app).
+
+### Getting started locally
+
+```shell
+npm install && npm start
+```
+
+Get first 3 starter Pokémon and their evolutions
+
+```shell
+curl -X GET http://localhost:9991/app/main/operations/Starters
+```
+
+---
+### Deploy to WunderGraph Cloud
+
+
+1. Fork this repo
+2. Sign in to [WunderGraph Cloud](https://cloud.wundergraph.com)
+3. Create a new project
+4. Import the forked repo
+5. Deploy the project
+
+---
+### Learn More
+
+Read the [Docs](https://wundergraph.com/docs).
+
