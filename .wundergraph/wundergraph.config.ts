8e6bd945107772dbe97fe7b494d620afd83edb7e
Nithin Kumar B <nithinkumar5353@gmail.com>
2022-11-03 14:24:01 +0530

    fix formatting

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -1,51 +1,52 @@
 import {
-    Application,
-    configureWunderGraphApplication,
-    cors,
-    EnvironmentVariable,
-    introspect,
-    templates,
-} from '@wundergraph/sdk';
-import server from './wundergraph.server';
-import operations from './wundergraph.operations';
+  Application,
+  configureWunderGraphApplication,
+  cors,
+  EnvironmentVariable,
+  introspect,
+  templates,
+} from "@wundergraph/sdk";
+import server from "./wundergraph.server";
+import operations from "./wundergraph.operations";
 
 const weather = introspect.graphql({
-    apiNamespace: 'weather',
-    url: 'https://graphql-weather-api.herokuapp.com/',
+  apiNamespace: "weather",
+  url: "https://graphql-weather-api.herokuapp.com/",
 });
 
 const myApplication = new Application({
-    name: 'app',
-    apis: [
-        weather,
-    ],
+  name: "app",
+  apis: [weather],
 });
 
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
-    application: myApplication,
-    server,
-    operations,
-    codeGenerators: [
-        {
-            templates: [
-                ...templates.typescript.all,
-                templates.typescript.operations,
-                templates.typescript.linkBuilder,
-            ],
-        },
-    ],
-    cors: {
-        ...cors.allowAll,
-        allowedOrigins:
-            process.env.NODE_ENV === 'production'
-                ? [
-                    // change this before deploying to production to the actual domain where you're deploying your app
-                    'http://localhost:3000',
-                ]
-                : ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+  application: myApplication,
+  server,
+  operations,
+  codeGenerators: [
+    {
+      templates: [
+        ...templates.typescript.all,
+        templates.typescript.operations,
+        templates.typescript.linkBuilder,
+      ],
     },
-    dotGraphQLConfig: {
-        hasDotWunderGraphDirectory: false,
-    },
+  ],
+  cors: {
+    ...cors.allowAll,
+    allowedOrigins:
+      process.env.NODE_ENV === "production"
+        ? [
+            // change this before deploying to production to the actual domain where you're deploying your app
+            "http://localhost:3000",
+          ]
+        : [
+            "http://localhost:3000",
+            new EnvironmentVariable("WG_ALLOWED_ORIGIN"),
+          ],
+  },
+  dotGraphQLConfig: {
+    hasDotWunderGraphDirectory: false,
+  },
 });
