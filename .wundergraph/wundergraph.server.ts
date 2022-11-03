e1668cb6cdf5d34d3d987ab93c43f7c5dfce77da
Nithin Kumar B <nithinkumar5353@gmail.com>
2022-11-03 14:23:54 +0530

    add post resolve hook

--- .wundergraph/wundergraph.server.ts
+++ .wundergraph/wundergraph.server.ts
@@ -1,11 +1,17 @@
-import {configureWunderGraphServer} from '@wundergraph/sdk';
-import type {HooksConfig} from './generated/wundergraph.hooks';
-import type {InternalClient} from './generated/wundergraph.internal.client';
+import { configureWunderGraphServer } from "@wundergraph/sdk";
+import type { HooksConfig } from "./generated/wundergraph.hooks";
+import type { InternalClient } from "./generated/wundergraph.internal.client";
 
 export default configureWunderGraphServer<HooksConfig, InternalClient>(() => ({
-    hooks: {
-        queries: {},
-        mutations: {},
+  hooks: {
+    queries: {
+      Weather: {
+        postResolve: async (hook) => {
+          hook.log.info("postResolve hook for Weather");
+        },
+      },
     },
-    graphqlServers: []
+    mutations: {},
+  },
+  graphqlServers: [],
 }));
